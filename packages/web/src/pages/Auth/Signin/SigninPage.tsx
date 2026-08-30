328412d5ecfae71dc2033eff9874564586066e8e
Matthew Warman <warmanmr@gmail.com>
2026-08-30 07:54:59 -0400

    feat: refactor sign in

--- packages/web/src/pages/Auth/Signin/SigninPage.tsx
+++ packages/web/src/pages/Auth/Signin/SigninPage.tsx
@@ -1,7 +1,8 @@
 import Page from '@react-starter/shared/components/Content/Page';
 import Container from '@react-starter/shared/components/Content/Container';
 import Heading from '@react-starter/shared/components/Text/Heading';
-import SigninForm from './components/SigninForm';
+
+import SigninForm from '@/pages/Auth/Signin/components/SigninForm';
 
 /**
  * The `SigninPage` component renders the content for a user authentication
@@ -10,10 +11,11 @@ import SigninForm from './components/SigninForm';
 const SigninPage = () => {
   return (
     <Page testId="page-signin">
-      <Container className="my-6 min-h-[50vh]">
-        <Heading level={1} className="mb-4 border-b border-neutral-500/50 pb-2">
+      <Container size="sm" className="min-h-[50vh]">
+        <Heading level={1} className="my-4">
           Sign In
         </Heading>
+
         <SigninForm />
       </Container>
     </Page>
