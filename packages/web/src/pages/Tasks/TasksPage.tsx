4c37b71f759682f6ed9dba27b76e63ef12f2bef2
Matthew Warman <warmanmr@gmail.com>
2026-09-05 07:10:04 -0400

    feat: removed user info card from tasks page

--- packages/web/src/pages/Tasks/TasksPage.tsx
+++ packages/web/src/pages/Tasks/TasksPage.tsx
@@ -10,7 +10,6 @@ import { Button } from '@react-starter/shared/components/shadcn/button';
 
 import { useGetCurrentUser } from '@/common/api/useGetCurrentUser';
 import TasksPageBreadcrumbs from '@/pages/Tasks/components/TasksPageBreadcrumbs';
-import UserInfo from '@/pages/Tasks/components/UserInfo';
 
 /**
  * The `TasksPage` component renders the layout for the tasks family of pages.
@@ -27,17 +26,10 @@ const TasksPage = ({ testId = 'page-tasks' }: PropsWithTestId) => {
         <TasksPageBreadcrumbs className="my-4" />
 
         {/* page heading */}
-        <div className="mb-4 flex items-center justify-between border-b border-neutral-500/50 pb-2">
+        <div className="my-8 flex items-center justify-between border-b pb-2">
           <Heading level={1}>{t('tasks', { ns: 'tasks' })}</Heading>
           <div className="flex items-center gap-4">
-            <Button
-              variant="outline"
-              size="icon"
-              className="rounded-full"
-              aria-label="Add Task"
-              data-testid={`${testId}-button-add`}
-              asChild
-            >
+            <Button variant="outline" size="icon" aria-label="Add Task" data-testid={`${testId}-button-add`} asChild>
               <Link to="/app/tasks/add" aria-label="Add Task">
                 <Plus />
               </Link>
@@ -47,8 +39,6 @@ const TasksPage = ({ testId = 'page-tasks' }: PropsWithTestId) => {
 
         {currentUser && (
           <div data-testid={`${testId}-content`}>
-            <UserInfo userId={currentUser.id} className="mb-4" />
-
             <Outlet />
           </div>
         )}
