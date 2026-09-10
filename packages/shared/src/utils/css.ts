cb268acf4b35933291bfc54ab00e252e97be24e0
Matthew Warman <warmanmr@gmail.com>
2026-09-10 06:18:03 -0400

    chore: shadcn migrate cn

--- packages/shared/src/utils/css.ts
+++ packages/shared/src/utils/css.ts
@@ -1,21 +1,2 @@
-/**
- * @module css
- * @description Utility functions for handling CSS class names, particularly in the context of Tailwind CSS.
- * This module provides a function to conditionally construct `className` strings and merge Tailwind CSS classes without conflicts.
- * It leverages the `clsx` library for conditional class name construction and the `tailwind-merge` library for merging Tailwind CSS classes.
- * @see {@link https://github.com/lukeed/clsx} for `clsx` documentation.
- * @see {@link https://github.com/dcastil/tailwind-merge} for `tailwind-merge` documentation.
- */
-
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-/**
- * Conditionally construct `className` strings. Merge Tailwind CSS classes
- * without conflicts.
- * @param inputs - Class values used to calculate the `className` value.
- * @returns {string} - A consolidated string containing CSS class names.
- */
-export const cn = (...inputs: ClassValue[]): string => {
-  return twMerge(clsx(inputs));
-};
+// Backwards compatibility for the `cn` utility function for shadcn.
+export { cn } from 'cn';
